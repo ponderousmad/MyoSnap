8adcb1fb5ab11cde8b30b77c96d98a13372f1f73
Adrian Smith <agnomen@gmail.com>
2016-09-26 09:10:23 -0400

    Formatting.

--- emg-data-sample.cpp
+++ emg-data-sample.cpp
@@ -38,10 +38,6 @@ private:
 
 class DataCollector : public myo::DeviceListener {
 public:
-    DataCollector()
-    {
-    }
-
     // onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
     void onUnpair(myo::Myo* myo, uint64_t timestamp)
     {
@@ -73,41 +69,43 @@ int main(int argc, char** argv)
     // We catch any exceptions that might occur below -- see the catch statement for more details.
     try {
 
-    // First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
-    // publishing your application. The Hub provides access to one or more Myos.
-    myo::Hub hub("com.example.emg-data-sample");
+        // First, we create a Hub with our application identifier.
+        // The Hub provides access to one or more Myos.
+        myo::Hub hub("com.ponderousmad.MyoSnap");
 
-    std::cout << "Attempting to find a Myo..." << std::endl;
+        std::cout << "Attempting to find a Myo..." << std::endl;
 
-    // Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
-    // immediately.
-    // waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
-    // if that fails, the function will return a null pointer.
-    myo::Myo* myo = hub.waitForMyo(10000);
+        // Next, we attempt to find a Myo to use.
+        // If a Myo is already paired in Myo Connect, this will return that Myo
+        // immediately.
+        // waitForMyo() takes a timeout value in milliseconds.
+        // In this case we will try to find a Myo for 10 seconds, and
+        // if that fails, the function will return a null pointer.
+        myo::Myo* myo = hub.waitForMyo(10000);
 
-    // If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
-    if (!myo) {
-        throw std::runtime_error("Unable to find a Myo!");
-    }
+        // If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
+        if (!myo) {
+            throw std::runtime_error("Unable to find a Myo!");
+        }
 
-    // We've found a Myo.
-    std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
+        // We've found a Myo.
+        std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
 
-    // Next we enable EMG streaming on the found Myo.
-    myo->setStreamEmg(myo::Myo::streamEmgEnabled);
+        // Next we enable EMG streaming on the found Myo.
+        myo->setStreamEmg(myo::Myo::streamEmgEnabled);
 
-    // Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
-    DataCollector collector;
+        // Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
+        DataCollector collector;
 
-    // Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
-    // Hub::run() to send events to all registered device listeners.
-    hub.addListener(&collector);
+        // Hub::addListener() takes the address of any object whose class inherits from DeviceListener,
+        // and will cause Hub::run() to send events to all registered device listeners.
+        hub.addListener(&collector);
 
-    // Finally we enter our main loop.
-    while (1) {
-        hub.run(1);
-        collector.print();
-    }
+        // Finally we enter our main loop.
+        while (1) {
+            hub.run(1);
+            collector.print();
+        }
 
     // If a standard exception occurred, we print out its message and exit.
     } catch (const std::exception& e) {
