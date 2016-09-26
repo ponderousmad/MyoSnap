05b2f37ccc5cf514c027819d71f93625dad95fee
Adrian Smith <agnomen@gmail.com>
2016-09-26 13:09:02 -0400

    Terminate capture with ctrl-C

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "CaptureEMG.h"
 
+#include <iostream>
+#include <signal.h>
+
+static volatile int sRunning = 1;
+
+void intHandler(int) {
+    sRunning = 0;
+}
+
 int main(int argc, char** argv)
 {
+    signal(SIGINT, intHandler);
+    
     CaptureEMG capture;
     if (capture.setup()) {
-        while(1) {
+        while(sRunning) {
             capture.update();
         }
     }
+    std::cout << "And done!" << std::endl << std::endl;
 }
